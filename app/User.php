1a111ed547447e3b480732f72aa227c0959054e0
alice <aditiyaivanpxrpl12018smkrus@gmail.com>
2021-02-24 14:09:29 +0700

    commit

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'roles', 'phone', 'address', 'token'
+        'email', 'password', 'username', 'roles', 'phone',  'token'
     ];
 
     /**
